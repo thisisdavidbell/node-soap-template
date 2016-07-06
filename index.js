1f8559f6109e2c417bca656f0dfdc960b1b47b7c
David Bell <thisisdavidbell@hotmail.com>
2016-07-06 16:00:58 +0100

    demonstrated correct xml parsing to fetch inputs from message body

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var handlebars = require('handlebars');
+var xmlparser = require('express-xml-bodyparser')
 
 var port = 80;
 var host = "localhost";
 var server = new express();
+
+server.use(xmlparser());
 
 server.post('/add', add_function);
 server.post('/subtract', subtract_function);
@@ -14,11 +17,37 @@ server.listen(port);
 console.log("Server running at " + host + ":" + port);
 
 function add_function(req, res) {
-  res.send("in add function");
+  res.send(req.body);
+// this acts the same, but you cant use quotes using this technique:  console.log(req.body.soapenvenvelope);
+  console.log(req.body);
+  console.log(req.body['soapenv:envelope']);
 };
 
 function subtract_function(req, res) {
-  res.send("in subtract function");
+  res.send("<some>in subtract function</some>");
+  console.log("#### req.body ####");
+  console.log(req.body);
+
+  console.log("\n#### req.body['soapenv:envelope']");
+  console.log(req.body['soapenv:envelope']);
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body']");
+  console.log(req.body['soapenv:envelope']['soapenv:body']);
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]");
+  console.log(req.body['soapenv:envelope']['soapenv:body'][0]);
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add']");
+  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add']);
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]");
+  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]);
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend']");
+  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend']);  
+
+  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend'][0]");
+  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend'][0]);  
 };
 
 function multiply_function(req, res) {
