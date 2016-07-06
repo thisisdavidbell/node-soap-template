9e62328f45b828c3d9d36a3b17cf0c336932bcc5
David Bell <thisisdavidbell@hotmail.com>
2016-07-06 13:48:36 +0100

    Adding all 4 maths routes with correct function hoisting

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ var port = 80;
 var host = "localhost";
 var server = new express();
 
-server.post('/add', function(req, res) {
-  res.send("in add function");
-});
+server.post('/add', add_function);
+server.post('/subtract', subtract_function);
+server.post('/multiply', multiply_function);
+server.post('/divide', divide_function);
 
 server.listen(port);
 console.log("Server running at " + host + ":" + port);
+
+function add_function(req, res) {
+  res.send("in add function");
+};
+
+function subtract_function(req, res) {
+  res.send("in subtract function");
+};
+
+function multiply_function(req, res) {
+  res.send("in multiply function");
+};
+
+function divide_function(req, res) {
+  res.send("in divide function");
+};
+
+
