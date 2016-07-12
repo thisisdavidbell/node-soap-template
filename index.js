ca25004d0b5b5b5ec7c333b9caf4b88aabbe6b61
David Bell <thisisdavidbell@hotmail.com>
2016-07-12 11:30:09 +0100

    working SOAP Service tested in SOAPUI

--- index.js
+++ index.js
@@ -13,57 +13,56 @@ server.post('/subtract', subtract_function);
 server.post('/multiply', multiply_function);
 server.post('/divide', divide_function);
 
+var response_template = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+  + '<soapenv:Body>'
+     + '<addResponse xmlns="http://math.pot.ibm.com">'
+        + '<addReturn xmlns="">{{result}}</addReturn>'
+     + '</addResponse>'
+  + '</soapenv:Body>'
++ '</soapenv:Envelope>';
+var template = handlebars.compile(response_template);
+
 server.listen(port);
 console.log("Server running at " + host + ":" + port);
 
 function add_function(req, res) {
-  res.send(req.body);
-// this acts the same, but you cant use quotes using this technique:  console.log(req.body.soapenvenvelope);
-  console.log(req.body);
-  console.log(req.body['soapenv:envelope']);
+  var vals = getinputs(req); 
+  var result = vals[0]+vals[1];
+
+  var data = { "result": result};
+  var soapresponse = template(data);
+
+  res.send(soapresponse);  
 };
 
 function subtract_function(req, res) {
   var vals = getinputs(req); 
-
   var result = vals[0]-vals[1];
 
+  var data = { "result": result};
+  var soapresponse = template(data);
 
-  res.send("<some>in subtract function</some> " + result);
-  
-
-/*  console.log("#### req.body ####");
-  console.log(req.body);
-
-  console.log("\n#### req.body['soapenv:envelope']");
-  console.log(req.body['soapenv:envelope']);
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body']");
-  console.log(req.body['soapenv:envelope']['soapenv:body']);
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]");
-  console.log(req.body['soapenv:envelope']['soapenv:body'][0]);
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add']");
-  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add']);
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]");
-  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]);
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend']");
-  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend']);  
-
-  console.log("\n#### req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend'][0]");
-  console.log(req.body['soapenv:envelope']['soapenv:body'][0]['math:add'][0]['augend'][0]);  
-*/  
+  res.send(soapresponse);  
 };
 
 function multiply_function(req, res) {
-  res.send("in multiply function");
+  var vals = getinputs(req); 
+  var result = vals[0]*vals[1];
+
+  var data = { "result": result};
+  var soapresponse = template(data);
+
+  res.send(soapresponse);  
 };
 
 function divide_function(req, res) {
-  res.send("in divide function");
+  var vals = getinputs(req); 
+  var result = vals[0]/vals[1];
+
+  var data = { "result": result};
+  var soapresponse = template(data);
+
+  res.send(soapresponse);  
 };
 
 function getinputs(req) {
